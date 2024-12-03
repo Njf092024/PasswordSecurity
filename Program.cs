b7439cb2f854852817569e00304ffacac86c140d
Njf092024 <Njf092024@gmail.com>
2024-12-03 09:44:31 +0100

    Add SHA-256 hashing for password input and display hashed output

--- Program.cs
+++ Program.cs
@@ -10,5 +10,8 @@ class Program
         Console.WriteLine("Enter your password");
         string? mockPassword = Console.ReadLine();
         Console.WriteLine($"Hello {mockUser}.");
+
+        string hashed = hashingExample.ComputeSHA256Hash(mockPassword);
+        Console.WriteLine($"Hello {mockUser}. Your password is: {hashed}");
     }
 }
