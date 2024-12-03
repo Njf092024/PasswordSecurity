e77a7043da03afb63c8bda9d804ecd72817f841d
Njf092024 <Njf092024@gmail.com>
2024-12-03 09:40:35 +0100

    Add user input for username and password with greeting functionality

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ class Program
     static void Main(string[] args)
     {
         HashingExample hashingExample = new HashingExample();
-        
+        Console.WriteLine("Enter your username");
+        string? mockUser = Console.ReadLine();
+        Console.WriteLine("Enter your password");
+        string? mockPassword = Console.ReadLine();
+        Console.WriteLine($"Hello {mockUser}.");
     }
 }
