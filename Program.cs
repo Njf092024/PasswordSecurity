04127919501eb18483e51a49a9213e45eb1e8a9e
Njf092024 <Njf092024@gmail.com>
2024-12-05 08:57:34 +0100

    Add placeholder for HashingExample class

--- Program.cs
+++ Program.cs
@@ -9,13 +9,22 @@ class Program
     static void Main(string[] args)
     {
         HashingExample hashingExample = new HashingExample();
+
         Console.WriteLine("Enter your username");
         string? mockUser = Console.ReadLine();
+
         Console.WriteLine("Enter your password");
         string? mockPassword = Console.ReadLine();
+
         Console.WriteLine($"Hello {mockUser}.");
 
-        string hashed = hashingExample.ComputeSHA256Hash(mockPassword);
+        string hashed = hashingExample.ComputeSHA256Hash(mockPassword ?? string.Empty);
+
         Console.WriteLine($"Hello {mockUser}. Your password is: {hashed}");
     }
+}
+
+public class HashingExample
+{
+
 }
